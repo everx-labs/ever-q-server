023d860d16b7f5f48e378cdc072bade5be6c2c99
Michael Vlasov <melsomino@gmail.com>
2019-06-25 13:42:27 +0200

    1) Add: arrays in matcher as an alternatives 2) Add: deep fields match

--- server/matcher.js
+++ server/matcher.js
@@ -1,16 +1,34 @@
+function valueMatched(match, value) {
+    if (match === null) {
+        return value === null || value === undefined;
+    }
+    if (Array.isArray(match)) {
+        for (const item of match) {
+            if (valueMatched(item, value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    if (typeof(match) === 'object') {
+        return matcher(match)(value);
+    }
+    return value === match;
+}
+
 function matcher(match) {
-	const keys = Object.keys(match);
-	if (keys.length === 0) {
-		return () => true;
-	}
-	return (doc) => {
-		for (const key of keys) {
-			if (doc[key] !== match[key]) {
-				return false;
-			}
-		}
-		return true;
-	};
+    const keys = Object.keys(match);
+    if (keys.length === 0) {
+        return () => true;
+    }
+    return (doc) => {
+        for (const key of keys) {
+            if (!valueMatched(match[key], doc[key])) {
+                return false;
+            }
+        }
+        return true;
+    };
 }
 
 
