0afd454b6d1a7e908dcab76a86483590a4fcf7fe
Alexander Novikov <alex.novikov.1990@gmail.com>
2022-06-06 11:09:47 +0300

    Add missing generated change

--- src/server/graphql/blockchain/resolvers-types-generated.ts
+++ src/server/graphql/blockchain/resolvers-types-generated.ts
@@ -376,6 +376,7 @@ export type BlockchainAccount = Node & {
     library?: Maybe<Scalars["String"]>
     /** `library` field root hash. */
     library_hash?: Maybe<Scalars["String"]>
+    prev_code_hash?: Maybe<Scalars["String"]>
     /** Merkle proof that account is a part of shard state it cut from as a bag of cells with Merkle proof struct encoded as base64. */
     proof?: Maybe<Scalars["String"]>
     /** Contains the number of public cells of the account. Used in storage fee calculation. */
@@ -2735,6 +2736,11 @@ export type BlockchainAccountResolvers<
     >
     library?: Resolver<Maybe<ResolversTypes["String"]>, ParentType, ContextType>
     library_hash?: Resolver<
+        Maybe<ResolversTypes["String"]>,
+        ParentType,
+        ContextType
+    >
+    prev_code_hash?: Resolver<
         Maybe<ResolversTypes["String"]>,
         ParentType,
         ContextType
